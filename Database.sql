64800bfda1b65a95a2a95fd6d46d02e424806179
developer <jheison540as@gmail.com>
2021-03-21 16:37:59 -0300

    Refatoracao do sql do banco

--- Database.sql
+++ Database.sql
@@ -2,51 +2,76 @@ DROP DATABASE ProjetoUnivesp2021;
 
 CREATE DATABASE IF NOT EXISTS ProjetoUnivesp2021;
 
-CREATE TABLE IF NOT EXISTS ProjetoUnivesp2021.HOSPITAL(
-   HID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-   HNOME VARCHAR(255) NOT NULL,
-   HUF TINYINT(2) NOT NULL,
-   HCIDADE VARCHAR(50) NOT NULL,
-   HCEP VARCHAR(8) NOT NULL,
-   HENDERECO VARCHAR(150) NOT NULL,
-   HCOMPLEMENTO VARCHAR(150),
-   HTELEFONE BIGINT(14) UNSIGNED NOT NULL,
-   HISPRONTOSOCORRO BOOLEAN NOT NULL DEFAULT FALSE,
-   HATIVADO ENUM('T', 'F', 'D') DEFAULT 'T'
+-- ProjetoUnivesp2021.HOSPITAL definition
+
+CREATE TABLE `HOSPITAL`
+(
+    `HID`              int(10) unsigned    NOT NULL AUTO_INCREMENT,
+    `HNOME`            varchar(255)        NOT NULL,
+    `HUF`              char(2)             NOT NULL,
+    `HCIDADE`          varchar(50)         NOT NULL,
+    `HCEP`             varchar(8)          NOT NULL,
+    `HENDERECO`        varchar(150)        NOT NULL,
+    `HCOMPLEMENTO`     varchar(150)                 DEFAULT NULL,
+    `HTELEFONE`        bigint(14) unsigned NOT NULL,
+    `HISPRONTOSOCORRO` tinyint(1)          NOT NULL DEFAULT 0,
+    `HATIVADO`         enum ('T','F','D')           DEFAULT 'T',
+    PRIMARY KEY (`HID`)
 );
 
-CREATE TABLE IF NOT EXISTS ProjetoUnivesp2021.ESPECIALIDADES(
-    EID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    HNOME VARCHAR(30) UNIQUE
+-- ProjetoUnivesp2021.ESPECIALIDADES definition
+
+CREATE TABLE `ESPECIALIDADES`
+(
+    `EID`   int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `HNOME` varchar(30) DEFAULT NULL,
+    PRIMARY KEY (`EID`),
+    UNIQUE KEY `HNOME` (`HNOME`)
 );
 
-CREATE TABLE IF NOT EXISTS ProjetoUnivesp2021.MEDICOS(
-    MID BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    MNOME VARCHAR(255) NOT NULL,
-    EID INT UNSIGNED NOT NULL,
-    HID INT UNSIGNED NOT NULL,
-    MATIVADO ENUM('T', 'F', 'D') DEFAULT 'T' NOT NULL
+-- ProjetoUnivesp2021.MEDICOS definition
+
+CREATE TABLE `MEDICOS`
+(
+    `MID`      bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `MNOME`    varchar(255)        NOT NULL,
+    `EID`      int(10) unsigned    NOT NULL,
+    `HID`      int(10) unsigned    NOT NULL,
+    `MATIVADO` enum ('T','F','D')  NOT NULL DEFAULT 'T',
+    PRIMARY KEY (`MID`)
 );
 
-CREATE TABLE IF NOT EXISTS ProjetoUnivesp2021.USUARIOS(
-    UID BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    UNOME VARCHAR(255) NOT NULL,
-    UEMAIL VARCHAR(100) NOT NULL UNIQUE,
-    UPASSWORD VARCHAR(80) NOT NULL,
-    UTOKEN CHAR(16) NOT NULL UNIQUE,
-    UCPF CHAR(11) NOT NULL UNIQUE,
-    UUF TINYINT(2) NOT NULL,
-    UCIDADE VARCHAR(50) NOT NULL,
-    UCEP VARCHAR(8) NOT NULL,
-    UENDERECO VARCHAR(150) NOT NULL,
-    UCOMPLEMENTO VARCHAR(150),
-    UATIVADO ENUM('T', 'F', 'D') DEFAULT 'T' NOT NULL
+-- ProjetoUnivesp2021.USUARIOS definition
+
+CREATE TABLE `USUARIOS`
+(
+    `UID`          bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `UNOME`        varchar(255)        NOT NULL,
+    `UEMAIL`       varchar(100)        NOT NULL,
+    `UPASSWORD`    varchar(80)         NOT NULL,
+    `UTOKEN`       char(16)            NOT NULL,
+    `UCPF`         char(11)            NOT NULL,
+    `UUF`          char(2)             NOT NULL,
+    `UCIDADE`      varchar(50)         NOT NULL,
+    `UCEP`         varchar(8)          NOT NULL,
+    `UENDERECO`    varchar(150)        NOT NULL,
+    `UCOMPLEMENTO` varchar(150)                 DEFAULT NULL,
+    `UATIVADO`     enum ('T','F','D')  NOT NULL DEFAULT 'T',
+    PRIMARY KEY (`UID`),
+    UNIQUE KEY `UEMAIL` (`UEMAIL`),
+    UNIQUE KEY `UTOKEN` (`UTOKEN`),
+    UNIQUE KEY `UCPF` (`UCPF`)
 );
 
-CREATE TABLE IF NOT EXISTS ProjetoUnivesp2021.AGENDAMENTOS(
-    AID BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    UID BIGINT UNSIGNED NOT NULL,
-    MID BIGINT UNSIGNED NOT NULL,
-    ADATA DATETIME NOT NULL UNIQUE,
-    AATIVADO ENUM('T', 'D') DEFAULT 'T' NOT NULL
-)
+-- ProjetoUnivesp2021.AGENDAMENTOS definition
+
+CREATE TABLE `AGENDAMENTOS`
+(
+    `AID`      bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `UID`      bigint(20) unsigned NOT NULL,
+    `MID`      bigint(20) unsigned NOT NULL,
+    `ADATA`    datetime            NOT NULL,
+    `AATIVADO` enum ('T','D')      NOT NULL DEFAULT 'T',
+    PRIMARY KEY (`AID`),
+    UNIQUE KEY `ADATA` (`ADATA`)
+);
