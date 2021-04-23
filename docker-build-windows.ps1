5d8aa5506703f2e7761e74c8996891c6c00e6c42
developer <jheison540as@gmail.com>
2021-04-23 20:52:21 -0300

    FIX docker build windows 2

--- docker-build-windows.ps1
+++ docker-build-windows.ps1
@@ -1,7 +1,8 @@
-docker rmi -f ProjetoUnivesp2021WEB:latest
-docker rm -f ProjetoUnivesp2021DB
-docker build -t ProjetoUnivesp2021WEB:latest .
-docker run -d -p 3306:3306 --name ProjetoUnivesp2021DB -e MYSQL_ROOT_PASSWORD=123  mariadb:latest
-docker cp Datbase.sql ProjetoUnivesp2021DB:/
-docker exec ProjetoUnivesp2021DB /bin/sh -c "mysql --host=127.0.0.1 --port=3306 -u root -p123 < /Database.sql"
-docker run docker run --it -e DATABASE_HOST="172.17.0.1" -e DATABASE_PORT="3306" -e DATABASE_USERNAME="root" -e DATABASE_PASSWORD="123" -e DATABASE_NAME="ProjetoUnivesp2021" -p 8080:8080 --name ProjetoUnivesp2021-WEB deletar:latest
+docker rmi -f projeto-univesp2021-web:latest
+docker rm -f projeto-univesp2021-db
+docker rm -f projeto-univesp2021-web
+docker run -d -p 3306:3306 --name projeto-univesp2021-db -e MYSQL_ROOT_PASSWORD=123 mariadb:latest
+docker cp Database.sql projeto-univesp2021-db:/
+docker build -t projeto-univesp2021-web:latest .
+docker exec projeto-univesp2021-db /bin/sh -c "mysql -u root -p123 < /Database.sql"
+docker run -it -e DATABASE_HOST="172.17.0.1" -e DATABASE_PORT="3306" -e DATABASE_USERNAME="root" -e DATABASE_PASSWORD="123" -e DATABASE_NAME="ProjetoUnivesp2021" -p 8080:8080 --name projeto-univesp2021-web projeto-univesp2021-web:latest
